a1b8d67ba69b0301317adc95283d724117cbb521
HaslinD <haslind@unitec.edu>
2017-02-10 14:28:43 -0600

    Tablero Creado

--- HaslinDAvila_-Lab4/src/haslindavila_/lab3/HaslinDAvila_Lab3.java
+++ HaslinDAvila_-Lab4/src/haslindavila_/lab3/HaslinDAvila_Lab3.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class HaslinDAvila_Lab3 {
-
+    static char[][] tableroA;
     private static String nombre;
     private static String nuser;
     private static int puntos;
@@ -15,13 +15,15 @@ public class HaslinDAvila_Lab3 {
 
     public static void main(String[] args) {
         ArrayList<jugadores> jugador = new ArrayList();
-
+        tableroA = new char[10][10]; 
         int opt = 0;
         do {
             String MenuP = "--Menu--\n"
                     + "1. Agregar\n"
                     + "2. Listar\n"
-                    + "3. Eliminar\n";
+                    + "3. Eliminar\n"
+                    + "4. Tablero\n"
+                    + "Ingrese la Opcion Siguiente: ";
             opt = Integer.parseInt(JOptionPane.showInputDialog(MenuP));
             switch (opt) {
                 case 1:
@@ -40,16 +42,43 @@ public class HaslinDAvila_Lab3 {
                             elementos += jugador.indexOf(t) + " " + ((jugadores) t) + "\n";
                         }
                     }
+                    JOptionPane.showMessageDialog(null, elementos);
                     break;
                 case 3:
+                    int pos = 0;
                     String elemento = "";
                     for (Object t : jugador) {
                         if (t instanceof jugadores) {
                             elemento += jugador.indexOf(t) + " " + ((jugadores) t) + "\n";
                         }
                     }
+                    pos = Integer.parseInt(JOptionPane.showInputDialog(elemento));
+                    jugador.remove(pos);
+                    break;
+                case 4:
+                    espacios();
+                    matriz(tableroA, 0 ,0);
                     break;
             }
         } while (opt > 0 && opt < 5);
+    }
+    public static void matriz(char[][] m, int f, int c){
+        if (f == m.length - 1 && c == m[0].length - 1)
+            System.out.println("[" + m[f][c] + "]");
+        else if (c == m[0].length - 1){
+            System.out.println("["+m[f][c]+"]");
+            matriz(m,f + 1,0);
+        } else {
+            System.out.print("[" + m[f][c] + "]");
+            matriz(m,f,c+1);
+        }
+    }
+    public static char[][] espacios(){
+        for (int i = 0; i < tableroA.length; i++) {
+            for (int j = 0; j < tableroA.length; j++) {
+                tableroA[i][j] = ' ';
+            }
+        }
+        return tableroA;
     }
 }
