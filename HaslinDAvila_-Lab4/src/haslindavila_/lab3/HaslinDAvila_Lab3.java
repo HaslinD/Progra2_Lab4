fe2e7f2513e2255932e1388ad65e7be2ab964543
franklin garcia <franklin__garcia@unitec.edu>
2017-02-10 14:05:01 -0600

    Listar

--- HaslinDAvila_-Lab4/src/haslindavila_/lab3/HaslinDAvila_Lab3.java
+++ HaslinDAvila_-Lab4/src/haslindavila_/lab3/HaslinDAvila_Lab3.java
@@ -1,33 +1,46 @@
 package haslindavila_.lab3;
 
 //Haslin David y Josue Garcia
-
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class HaslinDAvila_Lab3 {
+
     private static String nombre;
     private static String nuser;
     private static int puntos;
     private static String lugar_de_nacimiento;
     private static int edad;
     private static String genero;
+
     public static void main(String[] args) {
-        ArrayList<jugadores> jugador = new ArrayList(); 
-        
+        ArrayList<jugadores> jugador = new ArrayList();
+
         int opt = 0;
         do {
             String MenuP = "--Menu--\n"
                     + "1. Agregar\n"
-                    + "2. Modificar\n"
+                    + "2. Listar\n"
                     + "3. Eliminar\n";
             opt = Integer.parseInt(JOptionPane.showInputDialog(MenuP));
-            switch (opt){
+            switch (opt) {
                 case 1:
                     break;
                 case 2:
+                    String elementos = "";
+                    for (Object t : jugador) {
+                        if (t instanceof jugadores) {
+                            elementos += jugador.indexOf(t) + " " + ((jugadores) t) + "\n";
+                        }
+                    }
                     break;
                 case 3:
+                    String elemento = "";
+                    for (Object t : jugador) {
+                        if (t instanceof jugadores) {
+                            elemento += jugador.indexOf(t) + " " + ((jugadores) t) + "\n";
+                        }
+                    }
                     break;
             }
         } while (opt > 0 && opt < 5);
