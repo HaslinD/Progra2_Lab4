b067574e6be81af46383d076f6c1ee8c86949152
franklin garcia <franklin__garcia@unitec.edu>
2017-02-10 13:56:44 -0600

    nuevo

--- HaslinDAvila_-Lab4/src/haslindavila_/lab3/tablero.java
+++ HaslinDAvila_-Lab4/src/haslindavila_/lab3/tablero.java
@@ -11,7 +11,7 @@ import java.awt.Color;
  *
  * @author Franklin Garcia
  */
-public class tablero {
+public abstract class tablero  {
     protected Color color;
     protected String material;
    
